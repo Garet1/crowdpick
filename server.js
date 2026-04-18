7d431694839fc0fec65e69a07845794e8da6b78b
Garet <santi.delafuente1@gmail.com>
2026-04-18 09:48:55 -0300

    init: crowdpick fork from media-a-la-carta

---
+++ server.js
@@ -0,0 +1,105 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+const os = require('os');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+const VENUE_NAME = process.env.VENUE_NAME || 'Crowdpick';
+const PRIMARY_COLOR = process.env.PRIMARY_COLOR || '#6C63FF';
+
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
+const LOCAL_IP = getLocalIP();
+console.log(`IP local detectada: ${LOCAL_IP}`);
+console.log(`Venue: ${VENUE_NAME}`);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'vote.html'));
+});
+app.get('/display', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'display.html'));
+});
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+app.get('/api/ip', (req, res) => {
+  res.json({ ip: LOCAL_IP });
+});
+
+app.get('/api/config', (req, res) => {
+  res.json({ venueName: VENUE_NAME, primaryColor: PRIMARY_COLOR });
+});
+
+let currentPoll = null;
+let votes = {};
+let winnerVisible = false;
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado:', socket.id);
+  if (currentPoll) {
+    socket.emit('poll-update', { poll: currentPoll, votes });
+    if (winnerVisible) {
+      const winnerId = getWinnerId();
+      socket.emit('show-winner', { winnerId, poll: currentPoll, votes });
+    }
+  }
+
+  socket.on('start-poll', (poll) => {
+    currentPoll = poll;
+    votes = {};
+    winnerVisible = false;
+    poll.options.forEach(opt => votes[opt.id] = 0);
+    io.emit('poll-update', { poll: currentPoll, votes });
+  });
+
+  socket.on('vote', (optionId) => {
+    if (currentPoll && votes[optionId] !== undefined) {
+      votes[optionId]++;
+      io.emit('votes-update', votes);
+    }
+  });
+
+  socket.on('end-poll', () => {
+    winnerVisible = true;
+    const winnerId = getWinnerId();
+    io.emit('show-winner', { winnerId, poll: currentPoll, votes });
+  });
+
+  socket.on('reset-display', () => {
+    currentPoll = null;
+    votes = {};
+    winnerVisible = false;
+    io.emit('go-home');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+function getWinnerId() {
+  return Object.entries(votes).reduce((a, b) => b[1] > a[1] ? b : a, ['', -1])[0];
+}
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://${LOCAL_IP}:${PORT}`);
+});
